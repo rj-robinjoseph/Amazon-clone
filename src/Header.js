7cb24d8627f4f37965b372a4f58324ad586adcba
Robin Joseph <robinjoseph452@gmail.com>
2021-05-05 21:04:17 +0530

    Updated

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="https://www.pngitem.com/pimgs/b/1-12080_amazon-com-logo-png.png" />
+            <Link to="/"><img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" /></Link>
+            
 
             <div className='header__search'>
                    <input className="header__searchInput" type="text" />
@@ -28,15 +30,15 @@ function Header() {
                     <span className='header__optionLineOne'>Your</span>
                     <span className='header__optionLineTwo'>Prime</span>
                 </div>
-            <div className="header__optionBasket">
+
+                <Link to="/Checkout">
+                <div className="header__optionBasket">
                 <ShoppingBasketIcon />
                 <span className="header__optionLineTwo header__basketCount">0</span>
 
             </div>
-
-
-
-
+            </Link>
+ 
             </div>
         </div>
     )
