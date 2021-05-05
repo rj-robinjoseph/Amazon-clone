873c54393f319fc7be9945c074fbd2ad04202fd1
Robin Joseph <robinjoseph452@gmail.com>
2021-05-05 19:17:04 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     //BEM
+    <Router>
     <div className="App">
-      <Header />
-      <Home />
+
+    <Header />
+      <Switch>
+
+      <Route path="/Checkout">
+            
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            
+            <Home />
+          </Route>
+
+      </Switch>
+     
     </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Checkout.css";
+
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Checkout
