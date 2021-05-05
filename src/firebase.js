fccbdd350d97cc767df33278ddfb09d6686f0aae
Robin Joseph <robinjoseph452@gmail.com>
2021-05-05 16:44:03 +0530

    Building

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB3P0zcGxUEKEWITMwuXDdiDTtx5F7xRqc",
+    authDomain: "challenge-97b99.firebaseapp.com",
+    projectId: "challenge-97b99",
+    storageBucket: "challenge-97b99.appspot.com",
+    messagingSenderId: "1068457649534",
+    appId: "1:1068457649534:web:7014f1fd6a736bfaabbf2f",
+    measurementId: "G-68TKYGJR50"
+  };
